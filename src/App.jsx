756f9a54aeef0aac1a66be3878b2ec58781e9fc5
tnsilva <tnsilva175@gmail.com>
2022-10-22 21:25:03 -0300

    feat: add sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
 
+import styles from "./App.module.css";
+
 import "./global.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Lucas"
-        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae cupiditate nobis, facilis, fugit eum, doloremque voluptas sunt enim asperiores eaque accusantium beatae ad nesciunt similique ea explicabo officia reprehenderit dolores!"
-      />
-      <Post
-        author="Maria"
-        content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae cupiditate nobis, facilis, fugit eum, doloremque voluptas sunt enim asperiores eaque accusantium beatae ad nesciunt similique ea explicabo officia reprehenderit dolores!"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <Post
+            author="Lucas"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae cupiditate nobis, facilis, fugit eum, doloremque voluptas sunt enim asperiores eaque accusantium beatae ad nesciunt similique ea explicabo officia reprehenderit dolores!"
+          />
+          <Post
+            author="Maria"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae cupiditate nobis, facilis, fugit eum, doloremque voluptas sunt enim asperiores eaque accusantium beatae ad nesciunt similique ea explicabo officia reprehenderit dolores!"
+          />
+        </main>
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,26 @@
+import { PencilLine } from "phosphor-react";
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1607743386760-88ac62b89b8a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+      />
+
+      <div className={styles.profile}>
+        <img className={styles.avatar} src="https://github.com/tnsilva.png" />
+        <strong>Thalisson Sousa</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
